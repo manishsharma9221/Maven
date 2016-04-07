aaef92d35b6848db59c83feb725005e20dcebcac
Manish Sharma <manishsharma.9209@gmail.com>
2016-04-07 13:12:02 +0530

    "first commit"

---
+++ src/main/java/com/connecture/sample/Concatenation.java
@@ -0,0 +1,8 @@
+package com.connecture.sample;
+
+public class Concatenation {
+	public String concat(String s1, String s2) {
+		return s1 + s2;
+	    }
+
+}
---
+++ src/test/java/com/connecture/sample/ConcatenationTest.java
@@ -0,0 +1,13 @@
+package com.connecture.sample;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConcatenationTest {
+	 @Test
+	    public void testConcat() {
+		Concatenation myConcat = new Concatenation();
+	        String result = myConcat.concat("Hello", "World");
+	        assertEquals("HelloWorld", result);
+	    }
+}
